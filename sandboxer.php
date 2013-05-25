10f65b9d0a4f45f980188167ee2c11aa6b36bcaa
Ralf Albert <neun12@gmail.com>
2013-05-25 20:33:15 +0200

    create develop branch

--- sandboxer.php
+++ sandboxer.php
@@ -38,6 +38,7 @@ class Sandboxer {
 	public function output(){
 		echo '<div class="wrap">';
 		echo 'The GitHub Installer test plugin was successfully installed.';
+		echo '<p>This should be the <strong>develop</strong> branch</p>';
 		echo '</div>';
 	}
 }
